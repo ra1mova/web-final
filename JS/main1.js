0275db3ae09cbd90d7459628def7b8a1dd94673e
ra1mova <rraimova02@gmail.com>
2023-12-12 01:10:17 +0600

    Web final project

---
+++ JS/main1.js
@@ -0,0 +1,242 @@
+// Base URL for the Mock API
+const API_BASE_URL = 'https://65759ce4b2fbb8f6509d46ca.mockapi.io/todos/task';
+
+// Selectors
+const inputTask = document.querySelector("input");
+const inputDate = document.querySelector(".schedule-date");
+const buttonAdd = document.querySelector(".add-task-button");
+const listTodos = document.querySelector(".todos-list-body");
+const messageAlert = document.querySelector(".alert-message");
+const buttonDeleteAll = document.querySelector(".delete-all-btn");
+
+
+let taskList = [];
+let editMode = false;
+let idToEdit = null;
+
+// Retrieve tasks from API
+function retrieveTasks() {
+  fetch(API_BASE_URL)
+    .then(imgponse => {
+      if (!imgponse.ok) {
+        throw new Error(`HTTP error! Status: ${imgponse.status}`);
+      }
+      return imgponse.json();
+    })
+    .then(data => {
+      taskList = data;
+      renderTasks(data);
+    })
+    .catch(error => {
+      console.error('Error loading tasks:', error);
+      popupAlert("Error loading tasks: " + error.message, "error");
+    });
+}
+
+// On page load
+window.addEventListener("DOMContentLoaded", retrieveTasks);
+
+// Handle Add or Update task
+buttonAdd.addEventListener("click", () => {
+  if (editMode) {
+    submitTaskUpdate();
+  } else {
+    createNewTask();
+  }
+});
+
+inputTask.addEventListener("keyup", (e) => {
+  if (e.key === 'Enter' && inputTask.value.length > 0) {
+    if (editMode) {
+      submitTaskUpdate();
+    } else {
+      createNewTask();
+    }
+  }
+});
+
+// Create new task via API
+function createNewTask() {
+  const taskDetails = {
+    task: inputTask.value,
+    dueDate: inputDate.value,
+    completed: false,
+    status: "In progress",
+  };
+
+  fetch(API_BASE_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(taskDetails),
+  })
+  .then(imgponse => imgponse.json())
+  .then(() => {
+    popupAlert("Task successfully added", "success");
+    imgetInputs();
+    retrieveTasks();
+  })
+  .catch(error => console.error('Error adding task:', error));
+}
+
+// Update task in API
+function submitTaskUpdate() {
+  const updatedDetails = {
+    task: inputTask.value,
+    dueDate: inputDate.value,
+  };
+
+  fetch(`${API_BASE_URL}/${idToEdit}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedDetails),
+  })
+  .then(imgponse => imgponse.json())
+  .then(() => {
+    popupAlert("Task successfully updated", "success");
+    imgetInputs();
+    retrieveTasks();
+  })
+  .catch(error => console.error('Error updating task:', error));
+}
+
+// Enter edit mode
+function initiateEdit(id) {
+  const taskToEdit = taskList.find((task) => task.id === id);
+  inputTask.value = taskToEdit.task;
+  inputDate.value = taskToEdit.dueDate;
+
+  editMode = true;
+  idToEdit = id;
+  buttonAdd.textContent = 'Save Changes';
+}
+
+// imget form inputs
+function imgetInputs() {
+  inputTask.value = "";
+  inputDate.value = "";
+  editMode = false;
+  idToEdit = null;
+  buttonAdd.textContent = 'Add Task';
+}
+
+// Display alert message
+function popupAlert(message, type) {
+  let alertBox = `
+        <div class="alert alert-${type} shadow-lg mb-5 w-full">
+            <div>
+                <span>
+                    ${message}
+                </span>
+            </div>
+        </div>
+    `;
+  messageAlert.innerHTML = alertBox;
+  messageAlert.classList.remove("hide");
+  messageAlert.classList.add("show");
+  setTimeout(() => {
+    messageAlert.classList.remove("show");
+    messageAlert.classList.add("hide");
+  }, 3000);
+}
+
+// Remove task using API
+function removeTask(id) {
+  fetch(`${API_BASE_URL}/${id}`, {
+    method: 'DELETE',
+  })
+  .then(() => {
+    popupAlert("Task successfully removed", "success");
+    retrieveTasks();
+  })
+  .catch(error => console.error('Error removing task:', error));
+}
+
+// Toggle task completion status
+function changeTaskStatus(id) {
+  let taskToToggle = taskList.find((task) => task.id === id);
+  const updatedTask = {
+    ...taskToToggle,
+    completed: !taskToToggle.completed
+  };
+
+  fetch(`${API_BASE_URL}/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedTask),
+  })
+  .then(imgponse => imgponse.json())
+  .then(() => {
+    popupAlert("Task status changed", "success");
+    retrieveTasks();
+  })
+  .catch(error => console.error('Error changing task status:', error));
+}
+
+// Clear all tasks
+buttonDeleteAll.addEventListener("click", () => {
+  fetch(API_BASE_URL)
+    .then(imgponse => imgponse.json())
+    .then(data => {
+      Promise.all(data.map(task => 
+        fetch(`${API_BASE_URL}/${task.id}`, { method: 'DELETE' })
+      )).then(() => {
+        popupAlert("All tasks cleared", "success");
+        retrieveTasks();
+      });
+    })
+    .catch(error => console.error('Error clearing tasks:', error));
+});
+
+// Filter tasks based on status
+function filterTodos(status) {
+  let filteredTasks;
+  switch (status) {
+    case "all":
+      filteredTasks = taskList;
+      break;
+    case "pending":
+      filteredTasks = taskList.filter((task) => !task.completed);
+      break;
+    case "completed":
+      filteredTasks = taskList.filter((task) => task.completed);
+      break;
+    default:
+      return;
+  }
+  renderTasks(filteredTasks);
+}
+
+function renderTasks(tasksArray) {
+  listTodos.innerHTML = "";
+  if (tasksArray.length === 0) {
+    listTodos.innerHTML = `<tr><td colspan="5" class="text-center">No tasks available</td></tr>`;
+    return;
+  }
+  tasksArray.forEach((task) => {
+    listTodos.innerHTML += `
+            <tr class="task-row" data-id="${task.id}">
+                <td>${task.task}</td>
+                <td>${task.dueDate || "Not set"}</td>
+                <td>${task.completed ? "Completed" : "In progress"}</td>
+                <td>
+                    <button class="btn btn-modify btn-sm" onclick="initiateEdit('${task.id}')">
+                        <i class="bx bx-edit-alt bx-bx-xs"></i>    
+                    </button>
+                    <button class="btn btn-toggle btn-sm" onclick="changeTaskStatus('${task.id}')">
+                        <i class="bx bx-check bx-xs"></i>
+                    </button>
+                    <button class="btn btn-remove btn-sm" onclick="removeTask('${task.id}')">
+                        <i class="bx bx-trash bx-xs"></i>
+                    </button>
+                </td>
+            </tr>
+    `;
+  });
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# web-final
+
+---
+
+# Task Manager Application
+
+## Introduction
+The ToDo List application is an intuitive, web-based tool designed for efficiently managing daily tasks. Crafted using HTML, CSS, and JavaScript, it provides a straightforward interface for task management.
+
+## Usage
+- **Add Task**: Click on the input field to add a new task, specify the task details and due date, then pimgs the "Add" button.
+- **View Tasks**: Tasks are listed in an organized table format.
+- **Filter Tasks**: Use the filter options to view tasks based on their status (All, In progress, Completed).
+- **Delete Tasks**: You can delete individual tasks or use the "Clear All" button to remove all tasks.
+- **imgponsive Design**: The application is optimized for both desktop and mobile usage.
+
+## Customization
+You are encouraged to modify and customize the application to suit your preferences or specific requirements.
+
+## Contributing
+Contributions to improve the ToDo List application are welcomed. Feel free to fork the repository and submit pull requests.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+## Contact
+- **Developer**: Roza Raimova
+- **GitHub**: [https://github.com/ra1mova](https://github.com/ra1mova)
+- **Email**: roza.raimova@alatoo.edu.kg
+
+---
+
